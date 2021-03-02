49c9b2ba335428306fd69572ffedd9e479ccd6f6
Maria Jesus <bpmariajesus20@gmail.com>
2021-03-02 16:43:01 +0100

    He modificado el fichero codigo_coches.py

--- codigo_coches.py
+++ codigo_coches.py
@@ -1,4 +1,5 @@
 from lxml import etree
+doc = etree.parse('coches.xml')
 
 #Funcion: Listar información: Listar nombre y modelo de todos los coches.
 
@@ -25,4 +26,19 @@ def contar_coches_vendidos(doc):
 #Funcion: Buscar o filtrar información: Pedir por teclado una matricula y mostrar información del coche.
 
 def info_coche(doc,buscar_matricula):
+  informacion = doc.xpath('/coches/coche[buscar_matricula = "%s"]'%buscar_matricula)
+  return informacion 
+
+#Funcion: Buscar información relacionada: Pedir por teclado el nombre de un accesorio y mostrar la marca y el modelo.
+
+def info_coche_relacionada(doc,nombre_accesorio):
+  marca_coche = doc.xpath('/coches/coche/accesorio[nombre = nombre_accesorio]//../../marca/text()')
+  modelo_coche = doc.xpath('/coches/coche/accesorio[nombre = nombre_accesorio]//../../modelo/text()')
+  return zip(marca_coche,modelo_coche) 
+
+#Funcion: Ejercicio Libre: Pedir por teclado un modelo y mostrar sus accesorios.
+
+def accesorios_coche(doc,nombre_modelo):
+  accesorios_coche = doc.xpath('/coches/coche[modelo = nombre_modelo]/accesorio/nombre/text()')
+  return accesorios_coche
   
